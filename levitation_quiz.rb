bdd80976917a4b9b8909157c0977339dafd0fb3a
Daniel Ran <danielr1735@gmail.com>
2018-06-18 20:15:11 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,7 +1,7 @@
 
 def levitation_quiz
   answer = ''
-	while answer != "Wingarduio Leviosa"
+	while answer != "Wingardium Leviosa"
     puts "What is the spell that enacts levitation?"
     answer = gets.chomp
     break if answer == "Wingarduio Leviosa"
